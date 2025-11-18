b6e6c4bbe16762ecc32b799dc1ebed48b1ceeeec
cerise <c.druaertdupriez@ecoles-epsi.net>
2025-11-18 09:36:03 +0100

    création du site web base

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'header.php';
+?>
+<?php
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    } else {
+        $page = 'home';
+    }
+    if (file_exists($page . '.php')) {
+        require_once $page . '.php';
+    } else {
+        require_once 'error404.php';
+    }
+?>
+<?php
+require_once 'footer.php';
+?>
