cc28af2eddfdad023cfd0f4a7c88fdaede224fe6
corentin <corentin.pade.19@gmail.com  git config --global user.name corentingit config --global user.email corentin.pade.19@gmail.com>
2025-11-18 11:24:52 +0100

    modif

---
+++ fichier.php
@@ -0,0 +1,4 @@
+<div>
+      <label for="formFile" class="form-label mt-4">Default file input example</label>
+      <input class="form-control" type="file" id="formFile">
+    </div>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Boite livrable</title>
+    <title>Boite à livrable</title>
     <link href="https://bootswatch.com/5/pulse/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
@@ -32,7 +32,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">action</a>
             <a class="dropdown-item" href="#">Another action</a>
             <a class="dropdown-item" href="#">Something else here</a>
             <div class="dropdown-divider"></div>
