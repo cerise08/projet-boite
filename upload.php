dc5639c51e006f7e76579e845449376b2e2ad925
Corentin <corentin.pade.19@gmail.com>
2025-11-25 12:05:20 +0100

    coucou

--- upload.php
+++ upload.php
@@ -1,27 +1,34 @@
 <?php
-// Crée le dossier "uploads" s'il n'existe pas
-$uploadDir = 'uploads/';
-if (!is_dir($uploadDir)) {
-}
+// Vérifier si le formulaire a été soumis
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Vérifie si le fichier a été uploadé sans erreur.
+    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $filename = $_FILES["photo"]["name"];
+        $filetype = $_FILES["photo"]["type"];
+        $filesize = $_FILES["photo"]["size"];
 
-// Vérifie si un fichier a été envoyé
-if (isset($_FILES['fichier'])) {
-    $file = $_FILES['fichier'];
-    $targetFile = $uploadDir . basename($file['name']);
+        // Vérifie l'extension du fichier
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
 
-    // Vérifie qu'il n'y a pas d'erreur
-    if ($file['error'] === UPLOAD_ERR_OK) {
-        // Déplace le fichier depuis le dossier temporaire vers "uploads/"
-        if (move_uploaded_file($file['tmp_name'], $targetFile)) {
-            echo "Fichier téléchargé avec succès : " . htmlspecialchars($file['name']);
-        } else {
-            echo "Erreur lors de l'enregistrement du fichier.";
+        // Vérifie la taille du fichier - 5Mo maximum
+        $maxsize = 5 * 1024 * 1024;
+        if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
+
+        // Vérifie le type MIME du fichier
+        if(in_array($filetype, $allowed)){
+            // Vérifie si le fichier existe avant de le télécharger.
+            if(file_exists("upload/" . $_FILES["photo"]["name"])){
+                echo $_FILES["photo"]["name"] . " existe déjà.";
+            } else{
+                echo "Votre fichier a été téléchargé avec succès.";
+            } 
+        } else{
+            echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer."; 
         }
-    } else {
-        echo "Erreur d'upload : " . $file['error'];
+    } else{
+        echo "Error: " . $_FILES["photo"]["error"];
     }
-} else {
-    echo "Aucun fichier reçu.";
-
 }
 ?>
