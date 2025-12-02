e34e71053cae356985edb996ef08eab61bcf9126
cerise <c.druaertdupriez@ecoles-epsi.net>
2025-12-02 11:20:36 +0100

    changemement de la navbar selon si on est connecte ou non

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php
+// si la session n'est pas démarré alors le faire
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,29 +14,47 @@
   </head>
   <body>
   <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Boite à livrable</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.php?page=home">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?page=signup">Inscription</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?page=connexion">Connexion</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?page=fichier">Envoyer un fichier</a>
-        </li>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Boite à livrable</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+
+        <!-- Liens principaux à gauche -->
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php?page=home">Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <?php if (!isset($_SESSION['connected']) || $_SESSION['connected'] !== true): ?>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?page=signup">Inscription</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?page=connexion">Connexion</a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?page=fichier">Envoyer un fichier</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Déconnexion</a>
+            </li>
+          <?php endif; ?>
+        </ul>
+
+        <!-- Prénom à droite -->
+        <?php if (isset($_SESSION['connected']) && $_SESSION['connected'] === true): ?>
+          <span class="navbar-text fw-bold ms-auto">
+            Bonjour, <?php echo htmlspecialchars($_SESSION['prenom']); ?>
+          </span>
+        <?php endif; ?>
+
+      </div>
     </div>
-  </div>
-</nav>
-<div class="container text-center">
-  <div class="row align-items-start">
+  </nav>
+
+  <div class="container text-center">
+    <div class="row align-items-start">
